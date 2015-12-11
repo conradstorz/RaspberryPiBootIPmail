8c6d1e0966de9138af457f8ba388954cedff97b6
Conrad Storz <conradstorz@gmail.com>
2015-12-10 20:08:39 -0500

    added specific email addresses and application specific password

--- startup_mailer.py
+++ startup_mailer.py
@@ -1,3 +1,4 @@
+# /env/python
 """
 Script to generate an email containing the current IP address
 """
@@ -28,9 +29,9 @@ def connect_type(word_list):
 
 # Change to your own account information
 # Account Information
-to = 'username@email.com' # Email to send to.
-gmail_user = 'username@gmail.com' # Email to send from. (MUST BE GMAIL)
-gmail_password = 'gmailpassword' # Gmail password.
+mail_to = 'conradstorz@gmail.com' email to send to.
+gmail_user = 'estorz@gmail.com' # Email to send from. (MUST BE GMAIL)
+gmail_password = 'zrsmxrlimdzvzhqb' # Gmail application specific password
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use.
 
 smtpserver.ehlo()  # Says 'hello' to the server
@@ -68,7 +69,7 @@ my_ip_b = 'Your %s ip is %s' % (ip_type_b, ipaddr_b)
 msg = MIMEText(my_ip_a + "\n" + my_ip_b)
 msg['Subject'] = 'IPs For RaspberryPi on %s' % today.strftime('%b %d %Y')
 msg['From'] = gmail_user
-msg['To'] = to
+msg['To'] = mail_to
 # Sends the message
 smtpserver.sendmail(gmail_user, [to], msg.as_string())
 # Closes the smtp server.
