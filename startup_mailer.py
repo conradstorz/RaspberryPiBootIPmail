d7f55c1081c4505671d021b2e15d9bba889e81f5
Conrad Storz <conradstorz@gmail.com>
2015-12-12 12:52:44 -0500

    Update startup_mailer.py

--- startup_mailer.py
+++ startup_mailer.py
@@ -8,6 +8,25 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 80))
 print(s.getsockname()[0])
 s.close()
+
+and another...
+>>> import socket, struct, fcntl
+>>> sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+>>> sockfd = sock.fileno()
+>>> SIOCGIFADDR = 0x8915
+>>>
+>>> def get_ip(iface = 'eth0'):
+...     ifreq = struct.pack('16sH14s', iface, socket.AF_INET, '\x00'*14)
+...     try:
+...         res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
+...     except:
+...         return None
+...     ip = struct.unpack('16sH2x4s8x', res)[2]
+...     return socket.inet_ntoa(ip)
+... 
+>>> get_ip('eth0')
+'10.80.40.234'
+>>> 
 """
 
 __author__ = 'Cody Giles'
